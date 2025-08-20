0233d29c10b6d59a3c75ab82e73d087a522a4df1
VeronicaAlvez <vero.alvez@icloud.com>
2025-08-20 13:56:47 -0300

    Arreglos login

--- js/login.js
+++ js/login.js
@@ -1,16 +1,16 @@
-//Llama al botón
+// Agrega un event listener al formulario para el evento submit
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
     console.log("Form submitted");
     const username = document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
 //Simulación de credenciales
-    if (username != "vero" && password != "vero123") {
+    if (username != "vero" || password != "vero123") {
         alert("Credenciales incorrectas");
         return;
     }
 //Guarda los datos en localStorage
-    localStorage.setItem("session",{});
+    localStorage.setItem("session",JSON.stringify({ username }));
 
 //Redirecciona a index.html
     window.location.href = "index.html";
